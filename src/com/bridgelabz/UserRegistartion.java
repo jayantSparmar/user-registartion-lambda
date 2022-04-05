20689f772f9f66071c7eba0aa73c886040c4a501
jayant <jayantparmar420@gmail.com>
2022-04-05 11:40:18 +0530

    Added:"The First Name Validation in lambda expression program."

--- src/com/bridgelabz/UserRegistartion.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz;
-class UserRegistration {
-
-/**
- * Author:-Jayant Singh Parmar
- * Date:-04/04/2022
- */
-}
---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+import java.util.regex.Pattern;
+
+//Functional interface
+interface Validation {
+    String validate(String regex, String userDetails);
+}
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the lambda function validation");
+
+        Validation isValidFirstName = (pattern, firstName) -> {
+            return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
+        };
+        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Jayant"));
+
+    }
+}
